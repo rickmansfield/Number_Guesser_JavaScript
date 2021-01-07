190a542f27c9b4cf883f3af3265a6c94ffbeb13e
Rick <rmansfield@realpeoplesratings.com>
2021-01-07 17:52:15 -0600

    Add Saved Functions to Scripts

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+let generateTarget = () => {
+  return Math.floor(Math.random() * Math.floor(10));
+};
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+  return humanDifference <= computerDifference;
+}
+const alertUser = (human) => {
+  if (humanGuess > 9 || humanGuess < 0) {
+    return 'Number out of range'
+  }
+}
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
